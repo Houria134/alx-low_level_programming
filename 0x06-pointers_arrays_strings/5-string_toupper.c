271a2c06faa550f24041158a51503109647b92d8
Houria134 <h1344308@gmail.com>
2023-06-01 17:15:54 -0700

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase lett to uppercase
+ * @n: pointer
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+int h;
+h = 0;
+while (n[h] != '\0')
+{
+if (n[h] >= 'a' && n[h] <= 'z')
+n[h] = n[h] - 32;
+h++;
+}
+return (n);
+}
