e706416ceb4e0f54b5140d036847d510fdbb83c2
houria <h1344308@gmail.com>
2023-06-30 15:58:23 -0700

    task3

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,4 +20,5 @@ size_t print_list(const list_t *h);
 void free_list(list_t *head);
 size_t list_len(const list_t *head);
 list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 #endif
